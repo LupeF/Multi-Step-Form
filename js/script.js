a2840aa6eff01876eb9801a1f117eacfbb723c2d
LupeF <46634202+LupeF@users.noreply.github.com>
2024-02-28 12:41:08 -0800

    add two card containers with previous buttons

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const nameCard = document.querySelector('.name');
+const emailCard = document.querySelector('.email');
+const phoneCard = document.querySelector('.phone');
+const inputValue = document.getElementById('input-valuae');
+const nextBtn = document.querySelector('.next-button');
